7039fa18be62810a8e18b2a847be1832d820fbcb
ryanhburbank <ryanhburbank@gmail.com>
2014-01-26 01:36:13 -0800

    implement scratch each, map, sort_by, select, and reduce/inject, with tests

--- enumerables/enumerable.rb
+++ enumerables/enumerable.rb
@@ -7,24 +7,42 @@ module ScratchEnumerable
 	end
 
 	def my_map
-		output = []
-		each {|e| output << yield(e) }
-		output 
+		[].tap {|out| self.each {|e| out << yield(e) }}
 	end
 
-	# def my_select
-		# if block_given?
-		# 	self.each {|e| yield if e }
-		# else
-		# 	self
-		# end	
-	# end
+	def my_map!
+		self.each {|e| e = yield(e)}
+		self
+	end
 
-	# def my_sort_by
-	# end
+	def my_select
+		[].tap {|out| self.each {|e| out << e if yield(e)}}
+	end
 
-	# def my_reduce(*args)
-	# end
+	def my_sort_by
+		self.my_map {|a| [yield(a), a]}.sort.my_map {|a| a[1]}
+	end
+
+	def my_reduce(operation_or_value = nil)
+		case operation_or_value
+		when Symbol
+			return self.reduce {|s,e| s.send(operation_or_value, e)} 
+		when nil
+			accumulator_value = nil
+		else
+			accumulator_value = operation_or_value
+		end
+
+		self.each do |a|
+			if accumulator_value.nil?
+				accumulator_value = a
+			else
+				accumulator_value = yield(accumulator_value, a)
+			end
+		end
+
+		return accumulator_value
+	end
 
 	def to_s
 		"#<#{self.class} #{@object}:#{@method}>"
@@ -59,9 +77,9 @@ class SortedList
 	def each 
 		if block_given?
 			@elements.length.times { |index| yield(@elements[index]) }
-			@elements
+			return @elements
 		else
-			self
+			return ScratchEnumerator.new(self,:each)
 		end
 	end
 
--- enumerables/specs/scratch_enumerable_spec.rb
+++ enumerables/specs/scratch_enumerable_spec.rb
@@ -4,28 +4,48 @@ require_relative '../enumerable'
 describe "ScratchEnumerable" do
 	let(:array){ [1,2,3] }
 	let(:hash) { [foo: 1, oof: 2] }
-	let(:list) { SortedList.new([5,4,1])}
+	let(:list) { SortedList.new([-5,4,1])}
 
 	describe "instance methods" do
 		it "supports map" do
-			list.my_map { |e| e * e }.must_equal([1,16,25])
+			list.my_map { |e| e * e }.must_equal([25,1,16])
 		end
 
 		it "support select" do
-			skip
-			list.my_select { |e| e.even? }.must_equal([4,6])
+			list.my_select { |e| e.even? }.must_equal([4])
 		end
 
 		it "support sort_by" do
-			skip
-			list.my_sort_by {|e| x.to_s }.must_equal([4,5,6])
+			list.my_sort_by {|e| e.abs }.must_equal([1,4,-5])
 		end
 
 		it "supports reduce" do
-			skip
-			list.my_reduce(:+).must_equal(69)
-			list.my_reduce {|s,e| s + e }.must_equal(69)
-			list.my_reduce(-10) {|s,e| s + e }.must_equal(59)
+			list.my_reduce(:+).must_equal(0)
+			list.my_reduce {|s,e| s + e }.must_equal(0)
+			list.my_reduce(-10) {|s,e| s + e }.must_equal(-10)
 		end
 	end
+end
+
+describe "ScratchEnumerator" do 
+		let(:list) { SortedList.new(1,4,5)}
+
+		it "supports next" do
+			skip
+			enum = @list.each
+
+			enum.next.must_equal(1)
+			enum.next.must_equal(4)
+			enum.next.must_equal(5)
+
+			assert_raises(StopIteration) { enum.next }
+		end
+
+		it "supports with_index" do
+			skip
+			enum = @list.map
+			expected = ["0: 1","1: 4","2: 5"]
+
+			enum.with_index {|e,i| "#{i}: #{e}"}.must_equal(expected)
+		end
 end
